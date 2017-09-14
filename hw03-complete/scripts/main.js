1016f5dacf2df4bf5f4be0e2b97151c7087a747a
Miss-Megan <msmegannelson@gmail.com>
2017-09-14 01:29:47 -0700

    Add files via upload

---
+++ hw03-complete/scripts/main.js
@@ -0,0 +1,49 @@
+const beyonceTweet = {
+    name: 'Beyoncé',
+    handle: '@beyonce',
+    text: '#LEMONADE the Visual Album.'
+}
+
+const links = ['https://twitter.com/mmart168', 'https://twitter.com/beyonce', 'http://www.billboard.com/files/media/beyonce-2016-press-Parkwood-Entertainment-Lemonade-billboard-1548-650-4.jpg']
+
+const modifyText = () => {
+  document.querySelector('#message2').innerHTML = beyonceTweet.text
+  document.querySelector('#link2').innerHTML = beyonceTweet.name
+  document.querySelector('#user2').innerHTML = beyonceTweet.handle
+}
+
+
+const changeLinkAndImageAttributes = () => {
+  console.log('changing link attribute')
+  document.querySelector('#link1').href = links[0]
+  document.querySelector('#link2').href = links[1]
+  document.querySelector('#beyonce').src = links[2]
+
+}
+
+const changeClassAttribute = () => {
+  console.log('changing class attribute')
+  document.querySelector('.tweet').className = 'tweet-pink'
+  document.querySelector('#tweet1').className = 'tweet-pink'
+  document.querySelector('#tweet2').className = 'tweet-pink'
+}
+/*Update the appendElements function so that it adds the following elements
+ to the bottom of tweet1:
+
+An "a" element (a link) to the bottom of tweet1 that links to our course website
+(https://f17.ebook.mmart.us/) and has a link text that say "Intro to JavaScript."
+An "img" element (an image) that points to an image of your choice,
+symbolizing something about the course.
+
+GOAL: = <a href= https://f17.ebook.mmart.us> Intro to JavaScript</a>
+*/
+
+const appendElements = () => {
+  console.log('appending element')
+
+// YOUR CODE BELOW THIS LINE
+
+document.querySelector('#tweet1').innerHTML += '<a href= https://f17.ebook.mmart.us> Intro to JavaScript</a>'
+document.querySelector('#tweet1').innerHTML += '<img src= https://wpcdn.healthyplace.com/blogs/wp-content/uploads/2014/08/Overwhelmed-by-Stress-and-Anxiety-How-to-Deal-with-It.jpg></a>'
+
+}
